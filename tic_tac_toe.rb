c65d0fdc69158098c1521dca737bd3f6bb509f26
Ruiyotabby <ruiyotabby@gmail.com>
2022-09-24 19:49:46 +0300

    Add a class method which updates the display when a player chooses a box

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -15,22 +15,28 @@ class Player
 end
 
 class Game
-  @one = '_';@two = '_';@three = '_'
-  @four = '_';@five = '_';@six = '_'
-  @seven = '_';@eight = '_';@nine = '_'
 
   def self.start
-    puts "\t\tWelcome to Tic-Tac-toe"
-    puts "\t\t\t3  #{@seven}  #{@eight}  #{@nine}"
-    puts "\t\t\t2  #{@four}  #{@five}  #{@six}"
-    puts "\t\t\t1  #{@one}  #{@two}  #{@three}"
-    puts "\t\t\t   1  2  3"
+
+    self.display
 
     self.get_names
 
   end
 
   private
+
+  @one = '_'; @two = '_'; @three = '_'
+  @four = '_'; @five = '_'; @six = '_'
+  @seven = '_'; @eight = '_'; @nine = '_'
+
+  def self.display
+    puts "\t\tWelcome to Tic-Tac-toe"
+    puts "\t\t\t3  #{@seven}  #{@eight}  #{@nine}"
+    puts "\t\t\t2  #{@four}  #{@five}  #{@six}"
+    puts "\t\t\t1  #{@one}  #{@two}  #{@three}"
+    puts "\t\t\t   1  2  3"
+  end
 
   @player1 = ''
   @player2 = ''
@@ -61,16 +67,56 @@ class Game
   def self.get_input
     times = 9
     p1 = false
+    input = ''
 
     while times > 0
       if p1 == false
         print "\n#{@player1.name.chomp} to enter number of square to place a cross: "
+        input = gets
+        self.update_display(input.chomp,'X')
         p1 = !p1
+
       else
         print "\n#{@player2.name.chomp} to enter number of square to place a nought: "
+        input = gets
+        self.update_display(input.chomp,'O')
         p1 = !p1
       end
       times -= 1
+    end
+  end
+
+  def self.update_display(input,turn)
+    case input
+    when "1"
+      @one = turn
+      self.display
+    when "2"
+      @two = turn
+      self.display
+    when "3"
+      @three = turn
+      self.display
+    when "4"
+      @four = turn
+      self.display
+    when "5"
+      @five = turn
+      self.display
+    when "6"
+      @six = turn
+      self.display
+    when "7"
+      @seven = turn
+      self.display
+    when "8"
+      @eight = turn
+      self.display
+    when "9"
+      @nine = turn
+      self.display
+    else
+      puts 'Wrong input type please enter a numbet from 1 to 9'
     end
   end
 
