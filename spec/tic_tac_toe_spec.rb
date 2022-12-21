8841038022e0b49368a9b88873fb7097bfe008f6
Ruiyotabby <ruiyotabby@gmail.com>
2022-12-21 18:15:42 +0300

    Add a test suite

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,9 +2,15 @@ require './tic_tac_toe'
 
 describe Game do
   subject(:game_names) { described_class.new }
-  let(:player_names) { instance_double(Player) }
 
   describe '#get_names' do
-    
+    context 'when a user inputs name' do
+      it 'saves in name instance variable' do
+        allow(game_names).to receive(:gets).and_return('joe', 'james')
+        name = game_names.instance_variable_get(:@name)
+        expect(name).to eq 'joe'
+        game_names.get_names
+      end
+    end
   end
 end
