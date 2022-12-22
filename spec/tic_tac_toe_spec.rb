c99d417464b7735906da4d678d0f60987585d9b9
Ruiyotabby <ruiyotabby@gmail.com>
2022-12-22 10:22:26 +0300

    Add name length test to tic tac toe

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,14 +1,34 @@
 require './tic_tac_toe'
 
 describe Game do
-  subject(:game_names) { described_class.new }
 
   describe '#get_names' do
+    subject(:game_names) { described_class.new }
+
     context 'when a user inputs name' do
+      before do
+        allow(game_names).to receive(:puts)
+        allow(game_names).to receive(:print)
+        allow(game_names).to receive(:gets).and_return('joe')
+      end
       it 'saves in name instance variable' do
-        allow(game_names).to receive(:gets).and_return('joe', 'james')
+        game_names.get_names
         name = game_names.instance_variable_get(:@name)
         expect(name).to eq 'joe'
+      end
+    end
+
+    context 'when name.length is < 2' do
+      before do
+        allow(game_names).to receive(:puts).with('Each player must choose a name')
+        allow(game_names).to receive(:print).with('Enter a name: ')
+        invalid_input = 'k'
+        valid_input = 'kelly'
+        allow(game_names).to receive(:gets).and_return(invalid_input, invalid_input, valid_input)
+      end
+      it 'returns error twice' do
+        err = 'Failed! Name should be more than 1 character'
+        expect(game_names).to receive(:puts).with(err).twice
         game_names.get_names
       end
     end
